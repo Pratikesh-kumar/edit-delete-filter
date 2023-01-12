cdfdcba26cd3b341a1007d68d51484eb29601871
Pratikesh Kumar <122167622+Pratikesh-kumar@users.noreply.github.com>
2023-01-12 22:16:14 +0530

    Add files via upload
    filter

---
+++ filter.js
@@ -0,0 +1,20 @@
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+// Filter event
+filter.addEventListener('keyup', filterItems);
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get li
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
